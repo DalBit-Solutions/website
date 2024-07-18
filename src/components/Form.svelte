634b08a484bf37ea11475185134bd5c514ca041c
Alexander Fridlin <alex@fridlin.info>
2024-07-18 08:12:41 +0200

    form component

---
+++ src/components/Form.svelte
@@ -0,0 +1,27 @@
+<script>
+    export let name;
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+
+        const formElement = event.target;
+        const formData = new FormData(formElement);
+
+        const response = await fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: new URLSearchParams(formData).toString(),
+        });
+
+        if (response.ok) {
+            altert('success!');
+        }
+        else {
+            alert('fail')
+        }
+    }
+</script>
+
+<form {name} netlify on:submit={handleSubmit}>
+    <slot />
+</form>
