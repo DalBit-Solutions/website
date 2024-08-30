3dae77bf23a7b48003724c5a31a60f56cee6afc5
Alexander Fridlin <alex@fridlin.info>
2024-08-30 18:56:22 +0200

    made nav more accessible

--- src/components/Navigation.svelte
+++ src/components/Navigation.svelte
@@ -1,7 +1,15 @@
 <script>
     import dalBitLogoTextSvg from "../images/dalbit-logo-text.svg";
     export let menuItems = [];
+
+    let open = false;
+
+    function handleHashChange() {
+        open = false;
+    }
 </script>
+
+<svelte:window on:hashchange={handleHashChange} />
 
 <nav class="container">
     <ul>
@@ -19,19 +27,19 @@
     {#if menuItems.length > 0}
         <ul class="display-none-large">
             <li>
-                <input type="checkbox" id="menu-button" class="sr-only" />
-                <label for="menu-button">
-                    <span></span>
-                </label>
-                <aside>
-                    <nav class="container">
-                        <ul>
-                            {#each menuItems as item}
-                                <li><a href={item.href}>{item.label}</a></li>
-                            {/each}
-                        </ul>
-                    </nav>
-                </aside>
+                <details bind:open class="dropdown">
+                    <summary role="button" title="Menü öffnen">
+                        <span aria-hidden="true">&#9776;</span>
+                        <span class="sr-only">Menü öffnen</span>
+                    </summary>
+                    <ul dir="rtl">
+                        {#each menuItems as item}
+                            <li>
+                                <a href={item.href}>{item.label}</a>
+                            </li>
+                        {/each}
+                    </ul>
+                </details>
             </li>
         </ul>
         <ul class="display-none display-flex-large">
@@ -49,66 +57,13 @@
         }
     }
 
-    label[for="menu-button"] {
-        align-items: center;
-        cursor: pointer;
-        display: flex;
-        height: var(--menu-button-size);
-        position: relative;
-        width: var(--menu-button-size);
+    summary[role] {
+        font-size: var(--menu-button-size);
+        line-height: 1;
+        padding-top: 0;
     }
 
-    label[for="menu-button"]::before,
-    label[for="menu-button"] > span,
-    label[for="menu-button"]::after {
-        content: "";
-        width: var(--menu-button-size);
-        height: var(--menu-button-line-thickness);
-        background-color: var(--pico-color);
-        position: absolute;
-        left: 0;
-        transition: all var(--pico-transition);
-    }
-
-    label[for="menu-button"]::before {
-        top: 0;
-    }
-
-    label[for="menu-button"]::after {
-        bottom: 0;
-    }
-
-    #menu-button:checked ~ label[for="menu-button"]::before {
-        top: calc(var(--menu-button-size) / 2);
-        transform: rotate(45deg);
-        transform-origin: center;
-    }
-
-    #menu-button:checked ~ label[for="menu-button"] > span {
-        background-color: transparent;
-    }
-
-    #menu-button:checked ~ label[for="menu-button"]::after {
-        top: calc(var(--menu-button-size) / 2);
-        transform: rotate(-45deg);
-        transform-origin: center;
-    }
-
-    #menu-button:focus-visible ~ label[for="menu-button"] {
-        box-shadow: 0 0 0 var(--pico-outline-width) var(--pico-primary-focus);
-    }
-
-    #menu-button ~ aside {
-        display: none;
-    }
-
-    #menu-button:checked ~ aside {
-        background-color: var(--pico-background-color);
-        display: unset;
-        margin-top: var(--pico-nav-element-spacing-vertical);
-        position: absolute;
-        right: 0;
-        width: 100%;
-        z-index: 10;
+    summary[role]::after {
+        content: none;
     }
 </style>
