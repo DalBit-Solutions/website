fd69b625506a5b7e134938890c90b708ccf6b3d1
Alexander Fridlin <alex@fridlin.info>
2024-09-25 18:43:59 +0200

    added site map

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig } from 'astro/config';
 import { imageService } from "@unpic/astro/service";
-
 import svelte from "@astrojs/svelte";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   image: {
     service: imageService({
       placeholder: 'blurhash'
-    }),
+    })
   },
-  integrations: [svelte()]
+  integrations: [svelte(), sitemap()],
+  site: 'https://www.dalbit.de/'
 });
